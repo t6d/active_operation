f6128d586ea2ead4cefcf6f2312dda29bbcfce30
Konstantin Tennhard <konstantin@tennhard.net>
2016-09-01 19:58:12 -0400

    Specs for all early exit scenarios

--- spec/active_operation/base_spec.rb
+++ spec/active_operation/base_spec.rb
@@ -2,12 +2,14 @@ require 'spec_helper'
 
 describe ActiveOperation::Base do
   context "callbacks" do
+    STATES = [:before, :around, :execute, :after]
+
     subject :operation do
       Class.new(described_class) do
         attr_reader :log
 
-        property :halt_in, accepts: [:before, :around, :execute, :after]
-        property :succeed_in, accepts: [:before, :around, :execute, :after]
+        property :halt_in, accepts: STATES
+        property :succeed_in, accepts: STATES
 
         def initialize(*)
           super
@@ -15,28 +17,32 @@ describe ActiveOperation::Base do
         end
 
         before do
-          halt log if halt_in == :before
-          succeed log if succeed_in == :before
+          halt :before if halt_in == :before
+          succeed :before if succeed_in == :before
+
           log << :before
         end
 
         after do
-          halt log if halt_in == :after
-          succeed log if succeed_in == :after
+          halt :after if halt_in == :after
+          succeed :after if succeed_in == :after
+
           log << :after
         end
 
         around do |_, execute|
-          halt log if halt_in == :around
-          succeed log if succeed_in == :around
+          halt :around if halt_in == :around
+          succeed :around if succeed_in == :around
+
           log << :around_before
           execute.call
           log << :around_after
         end
 
         def execute
-          halt log if halt_in == :execute
-          succeed log if succeed_in == :execute
+          halt :execute if halt_in == :execute
+          succeed :execute if succeed_in == :execute
+
           log << :execute
           log
         end
@@ -54,26 +60,28 @@ describe ActiveOperation::Base do
       ])
     end
 
-    it "should support early exit in the before filters" do
-      log = operation.perform(halt_in: :before)
-      expect(log).to eq([])
-    end
+    STATES.each do |state|
+      it "should support halting in #{state} statements when executed using #perform" do
+        output = operation.perform(halt_in: state)
+        expect(output).to eq(state)
+      end
 
-    it "should support early exit in the before filters" do
-      operation_instance = operation.run(halt_in: :before)
-      expect(operation_instance.output).to eq([])
-      expect(operation_instance).to be_halted
-    end
+      it "should support succeeding in #{state} statements when executed using #perform" do
+        output = operation.perform(succeed_in: state)
+        expect(output).to eq(state)
+      end
 
-    it "should support early exit in the before filters" do
-      operation_instance = operation.run(succeed_in: :before)
-      expect(operation_instance.output).to eq([])
-      expect(operation_instance).to be_succeeded
-    end
+      it "should support halting in #{state} statements when executed using #run" do
+        operation_instance = operation.run(halt_in: state)
+        expect(operation_instance.output).to eq(state)
+        expect(operation_instance).to be_halted
+      end
 
-    it "should support early exit in the around filters" do
-      log = operation.perform(halt_in: :around)
-      expect(log).to eq([:before])
+      it "should support succeeding in #{state} statements when executed using #run" do
+        operation_instance = operation.run(succeed_in: state)
+        expect(operation_instance.output).to eq(state)
+        expect(operation_instance).to be_succeeded
+      end
     end
   end
 end
