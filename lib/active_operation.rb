d645b14fef57588a4a8e1d3a31896ba7d10faecc
Konstantin Tennhard <konstantin@tennhard.net>
2016-09-05 15:09:27 -0400

    Default terminator
    Removes `ActiveOperation.terminator` in favour of `ActiveSupport::Callbacks.default_terminator` by replacing the symbol that currently thrown with `:abort`.

--- lib/active_operation.rb
+++ lib/active_operation.rb
@@ -4,17 +4,6 @@ require "smart_properties"
 module ActiveOperation
   class Error < RuntimeError; end
   class AlreadyCompletedError < Error; end
-
-  def self.terminator
-    ->(target, result_lambda) {
-      terminate = true
-      catch(:interrupt) do
-        result_lambda.call
-        terminate = false
-      end
-      terminate
-    }
-  end
 end
 
 require "active_operation/version"
